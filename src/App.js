677122652bbf6a15c2855f2ca1818fe73a4b4511
Stachacl <stacha.l@icloud.com>
2022-11-26 21:34:32 +1100

    login window form added

--- src/App.js
+++ src/App.js
@@ -9,7 +9,19 @@ function App() {
         <h1> Login </h1>
         <div className="container">
           <div className="top">
-
+            <i class="fab fa-google"/>
+            <i class="fab fa-twitter"/>
+            <p className="divider">or</p>
+            <form>
+              <label> E-mail </label>
+              <input type='email' placeholder="Enter your email"/>
+              <label> Password </label>
+              <input type='password' placeholder="Enter yourPassword"/>
+              <div className="remember-me">
+                <input type='checkbox' checked='checked'/>
+                <p> Remember Me </p>
+              </div>
+            </form>
           </div>
         </div>
       </div>

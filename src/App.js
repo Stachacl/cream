3f4c45bce35820bf23e131fe8e34577abb60f0e7
Stachacl <stacha.l@icloud.com>
2022-11-27 10:12:50 +1100

    css styles applied

--- src/App.js
+++ src/App.js
@@ -4,38 +4,41 @@ import "./index.css";
 
 function App() {
   return (
-    <div className="app">
-      <div className="login">
-        <h1> Login </h1>
-        <div className="container">
-          <div className="top">
-            <i class="fab fa-google"/>
-            <i class="fab fa-twitter"/>
-            <p className="divider">or</p>
-            <form>
-              <label> E-mail </label>
-              <input type='email' placeholder="Enter your email"/>
-              <label> Password </label>
-              <input type='password' placeholder="Enter yourPassword"/>
-              <div className="remember-me">
-                <input type='checkbox' checked='checked'/>
-                <p> Remember Me </p>
-              </div>
-              <button>Log In</button>
-            </form>
-            <div className="bottom">
-              <p>Forget your password?</p>
-              <a href="/">Reset Password</a>
-            </div>
-            <a href="/" className="create-account"> Create Account</a>
-          </div>
-          <div className="theme-toggle">
-            <h2> Light Theme</h2>
-            <i class='fas fa-toggle-on'/>
-          </div>
+    <div className="app" >
+    <div className='login'>
+      <h1> Login</h1>
+      <div className='container'>
+        <div className='top'>
+          <i class='fab fa-google'></i>
+          <p> Log in with existing Google account</p>
+  
+          {/* <i class='fab fa-facebook-square'></i>
+          <i class='fab fa-apple'></i> */}
         </div>
+        <p className='divider'><span>Or</span></p>
+        <form>
+          <label>E-mail</label>
+          <input type='email' placeholder='Enter your email' />
+          <label>Password</label>
+          <input type='password' placeholder='Enter your password' />
+          <div className='remember-me'>
+            <input type='checkbox' checked='checked' />
+            <p>Remember Me</p>
+          </div>
+          <button>Log In</button>
+        </form>
+        <div className='bottom'>
+          <p>Forget your password?</p>
+          <a href='/'>Reset Password</a>
+        </div>
+        <p className='create-account'>Create Account</p>
+      </div>
+      <div className='theme-toggle'>
+        <h2>Light Theme</h2>
+        <i class='fas fa-toggle-on'></i>
       </div>
     </div>
+  </div>
   );
 }
 

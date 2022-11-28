b8d62afc6cbdd77b4bee839f6dd0a1a9a414098a
Stachacl <stacha.l@icloud.com>
2022-11-28 21:41:02 +1100

    single cream components created

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="container mx-auto max-w-2xl pt-5">
-      <h1 className="text-3xl font-bold "> Cream App 2</h1>
+      <h1 className="text-3xl font-bold "> Cream App </h1>
 
     </div>
   
---
+++ src/components/SingleCream.tsx
@@ -0,0 +1,11 @@
+import PropTypes from 'prop-types'
+
+function SingleCream() {
+  return (
+    <div>SingleCream</div>
+  )
+}
+
+SingleCream.propTypes = {}
+
+export default SingleCream

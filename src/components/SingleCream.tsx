9de6c72e3b9b7ad033672fb4ae6594fb62bbd0bf
Stachacl <stacha.l@icloud.com>
2022-11-29 21:19:47 +1100

    function show describtion added

--- src/components/SingleCream.tsx
+++ src/components/SingleCream.tsx
@@ -1,18 +1,28 @@
+import { useState } from "react";
 import { ICream } from "../models";
 
 interface SingleCreamProps {
   cream: ICream;
 }
 
-export function SingleCream( { cream }: SingleCreamProps) {
+export function SingleCream({ cream }: SingleCreamProps) {
+  const [ descriptionDisplay, setDescriptionDisplay ] = useState(false)
 
   return (
     <div className="border py-2 px-4 rounded flex flex-col items-center mb-2">
-      <img src={cream.image} className="w-1/3" alt={cream.title}/>
-      <p> {cream.title }</p>
-      <button className="py-2 px-4 border bg-slate-300"> Show describtion </button>
+      <img src={cream.image} className="w-1/3" alt={cream.title} />
+      <p> {cream.title}</p>
       <span className="font-bold"> ${cream.price} </span>
 
+      <button 
+        className="py-2 px-4 border bg-slate-300"
+        >
+        Show description
+      </button>
+
+      <div className="border bg-slate-100 py-3 my-3">
+        <p>{cream.description}</p>
+      </div>
     </div>
   );
 }

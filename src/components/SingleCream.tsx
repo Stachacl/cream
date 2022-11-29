171f6fca7fb3a1a9c679c63e91a8aed0c34adf7b
Stachacl <stacha.l@icloud.com>
2022-11-29 20:35:04 +1100

    image added to single cream componet

--- src/components/SingleCream.tsx
+++ src/components/SingleCream.tsx
@@ -9,6 +9,7 @@ export function SingleCream(props: SingleCreamProps) {
   return (
     <div className="border py-2 px-4 rounded flex flex-col items-center mb-2">
       {props.cream.title }
+      <img src={props.cream.image}/>
     </div>
   );
 }

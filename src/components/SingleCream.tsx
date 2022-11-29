3491316885b5c28eaf91acf6d6ea59c0a3e969e5
Stachacl <stacha.l@icloud.com>
2022-11-29 20:39:38 +1100

    image class added and alt describtion

--- src/components/SingleCream.tsx
+++ src/components/SingleCream.tsx
@@ -9,7 +9,7 @@ export function SingleCream( { cream }: SingleCreamProps) {
   return (
     <div className="border py-2 px-4 rounded flex flex-col items-center mb-2">
       {cream.title }
-      <img src={cream.image}/>
+      <img src={cream.image} className="w-1/6" alt={cream.title}/>
     </div>
   );
 }

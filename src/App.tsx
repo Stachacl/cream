fbc307872bacd2992964e6b6e16c49f26face2ab
Stachacl <stacha.l@icloud.com>
2022-11-29 20:32:24 +1100

    one more cream component added to app

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ function App() {
     <div className="container mx-auto max-w-2xl pt-5">
       <h1 className="text-3xl font-bold "> Cream App </h1>
       <SingleCream cream={creams[0]} />
+      <SingleCream cream={creams[1]} />
     </div>
   );
 }

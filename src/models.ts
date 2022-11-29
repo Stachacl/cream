a8715f7297858250f78de9a3274d80c555e0a408
Stachacl <stacha.l@icloud.com>
2022-11-29 19:43:34 +1100

    listed all typed of cream data in interface icream

--- src/models.ts
+++ src/models.ts
@@ -1,5 +1,12 @@
-
 export interface ICream {
-  
-  
+  id: number
+  title: string
+  price: number
+  description: string
+  category: string
+  image: string
+  rating: {
+    rate: number
+    count: number
+  }
 }

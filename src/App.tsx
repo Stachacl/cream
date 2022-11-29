609b1594ade94ec1dbe4de81ed0830b65300f43e
Stachacl <stacha.l@icloud.com>
2022-11-29 19:39:49 +1100

    models file created

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import { SingleCream } from "./components/SingleCream";
-import { creams } from "./data/creams"
+import { creams } from "./data/creams";
 
 function App() {
   return (
     <div className="container mx-auto max-w-2xl pt-5">
       <h1 className="text-3xl font-bold "> Cream App </h1>
-      <SingleCream cream={ creams[0]} />
+      <SingleCream cream={creams[0]} title={"test title"} />
     </div>
-  
-  )
-  
+  );
 }
 
 export default App;

9b6b62e2da519191a00b21ab04179818804d0259
Stachacl <stacha.l@icloud.com>
2022-11-29 20:38:09 +1100

    added cream to props so i dont need to write props.cream every time, but just cream

--- src/components/SingleCream.tsx
+++ src/components/SingleCream.tsx
@@ -4,12 +4,12 @@ interface SingleCreamProps {
   cream: ICream;
 }
 
-export function SingleCream(props: SingleCreamProps) {
+export function SingleCream( { cream }: SingleCreamProps) {
 
   return (
     <div className="border py-2 px-4 rounded flex flex-col items-center mb-2">
-      {props.cream.title }
-      <img src={props.cream.image}/>
+      {cream.title }
+      <img src={cream.image}/>
     </div>
   );
 }

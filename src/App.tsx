efa0ef09fffef1eb229a8509088256177926472b
Stachacl <stacha.l@icloud.com>
2022-11-30 10:38:30 +1100

    code in app optimised

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,13 @@ function App() {
   return (
     <div className="container mx-auto max-w-2xl pt-5">
       <h1 className="text-3xl font-bold "> Cream App </h1>
-      <SingleCream cream={creams[0]} />
-      <SingleCream cream={creams[1]} />
+
+      {creams.map(cream => < SingleCream cream={cream}/>)}
+      
+      {/* same code before optimisation below: */}
+
+      {/* <SingleCream cream={creams[0]} />
+      <SingleCream cream={creams[1]} /> */}
     </div>
   );
 }

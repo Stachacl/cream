f43855b5526d5d9b8465b509c2495eaeef9407d9
Stachacl <stacha.l@icloud.com>
2022-11-29 20:30:23 +1100

    one more cream info added to data file

--- src/data/creams.ts
+++ src/data/creams.ts
@@ -11,4 +11,14 @@ export const creams: ICream[] = [
     image: "https://www.davidjones.com/productimages/medium/1/2450768_21881141_8176534.jpg",
     rating: { rate: 4.9, count: 120 },
   },
+  {
+    id: 2,
+    title: "Yves Saint Laurent",
+    price: 510.00,
+    description:
+      "An ultra-sensorial journey experienced thanks to an exceptional transformative texture for visible youth reignition in just 4 hours. Infused with the extraordinary Saffron Pistil Extract, rich in millions of subcellular fractions, Or Rouge La Creme Essentielle visibly reignites the skin's appearance. From the first application, skin is immediately more radiant and smoother. In just four hours, skin's youth is visibly reignited, as it becomes firmer and tighter. Day after day, signs of age are reduced and skin feels more comfortable.",
+    category: "moisturisers",
+    image: "https://www.davidjones.com/productimages/medium/1/2473373_21983867_8344498.jpg",
+    rating: { rate: 4.5, count: 103 },
+  },
 ];

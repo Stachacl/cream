837b90cb3c0d4d6fc739c5043dbb0a66a9ffbfbc
Stachacl <stacha.l@icloud.com>
2023-01-22 22:35:01 +1100

    Hamburger menu item added

---
+++ src/components/IconHamburger.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const HamburgerIcon = () => (
+  <div className="block lg:hidden">
+    <button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+      <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>More</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+    </button>
+  </div>
+)
+export default HamburgerIcon

aa855350cf40c69e0ed99e1772bb0212216265e3
Stachacl <stacha.l@icloud.com>
2023-01-22 22:13:12 +1100

    styled navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,18 +2,18 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 
-
 interface Props {}
 
 const Navbar: React.FC<Props> = () => {
   return (
-    <nav>
-      <Link to="/all">All</Link>
-      <Link to="/blacklist">Blacklist</Link>
-      <Link to="/whitelist">Whitelist</Link>
-      <Link to="/my-allergens">My Allergens</Link>
+    <nav className="bg-gray-800 text-white p-2">
+      <Link className="px-2 py-1 rounded-lg hover:bg-gray-700" to="/all">All</Link>
+      <Link className="px-2 py-1 rounded-lg hover:bg-gray-700" to="/blacklist">Blacklist</Link>
+      <Link className="px-2 py-1 rounded-lg hover:bg-gray-700" to="/whitelist">Whitelist</Link>
+      <Link className="px-2 py-1 rounded-lg hover:bg-gray-700" to="/my-allergens">My Allergens</Link>
     </nav>
   );
 };
+
 
 export default Navbar;
